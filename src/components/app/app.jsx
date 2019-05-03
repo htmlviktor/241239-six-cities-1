44c3a61892d453cccc53edc1e7062bc2bf94c2fc
viktorkan <viktorkan9@gmail.com>
2019-05-03 22:54:54 +0300

    create new components and add propTypes validation

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainPage from '../main-page/main-page.jsx';
 
-const App = () => {
-  return <MainPage />;
+const App = (props) => {
+  const {data} = props;
+  return <MainPage
+    data={data}
+  />;
+};
+
+App.propTypes = {
+  data: PropTypes.array
 };
 
 export default App;
---
+++ src/components/main-card/main-card.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ItemCard from '../item-card/item-card.jsx';
+
+const MainCard = (props) => {
+  const {infoCard} = props;
+  const cards = infoCard.map((el, index) => {
+    return <ItemCard
+      title={el.title}
+      key={index}
+    />;
+  });
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {cards}
+    </div>
+  );
+};
+
+MainCard.propTypes = {
+  infoCard: PropTypes.array
+};
+
+export default MainCard;
