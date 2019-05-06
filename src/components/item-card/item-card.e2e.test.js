6b65f05817edbdf81c6f4a465fb44601c02728ed
viktorkan <viktorkan9@gmail.com>
2019-05-06 14:58:36 +0300

    add describe for e2e tests

--- src/components/item-card/item-card.e2e.test.js
+++ src/components/item-card/item-card.e2e.test.js
@@ -5,30 +5,24 @@ import ItemCard from './item-card.jsx';
 
 Enzyme.configure({adapter: new Adapter()});
 
-it(`Click on link - 'Head in Card'`, () => {
+describe(`Test e2e for ItemCard`, () => {
   const clickHandler = jest.fn();
   const app = shallow(<ItemCard
     title = {`Hello world`}
-    onClickTitle = {clickHandler}
-  />);
+    onClickTitle = {clickHandler}/>);
 
-  const titleLink = app.find(`.place-card__name a`);
-  titleLink.simulate(`click`, {preventDefault() {}});
+  it(`Click on link - 'Head in Card'`, () => {
+    const titleLink = app.find(`.place-card__name a`);
+    titleLink.simulate(`click`, {preventDefault() {}});
 
+    expect(clickHandler).toHaveBeenCalledTimes(1);
+  });
 
-  expect(clickHandler).toHaveBeenCalledTimes(1);
-});
+  it(`Click on link - 'Image in Card'`, () => {
 
-it(`Click on link - 'Image in Card'`, () => {
-  const clickHandler = jest.fn();
-  const app = shallow(<ItemCard
-    title = {`Hello world`}
-    onClickImage = {clickHandler}
-  />);
+    const imageLink = app.find(`.place-card__image-wrapper a`);
+    imageLink.simulate(`click`, {preventDefault() {}});
 
-  const imageLink = app.find(`.place-card__image-wrapper a`);
-  imageLink.simulate(`click`, {preventDefault() {}});
-
-
-  expect(clickHandler).toHaveBeenCalledTimes(1);
+    expect(clickHandler).toHaveBeenCalledTimes(1);
+  });
 });
