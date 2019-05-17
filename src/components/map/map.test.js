df371a20b085d444eafd79133351f7b783e06449
viktorkan <viktorkan9@gmail.com>
2019-05-17 22:56:00 +0300

    create snapshoot Map component

---
+++ src/components/map/map.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Map from './map.jsx';
+
+const mock = [
+  {
+    title: `Beautiful & luxurious apartment at great location`,
+    price: `120`,
+    degree: `Apartment`,
+    order: `Premium`,
+    photo: `img/apartment-01.jpg`,
+    id: 1,
+    coordinates: [52.3909553943508, 4.85309666406198],
+  },
+];
+
+it(`Snapshot test Map Component`, () => {
+  const tree = renderer
+  .create(<Map
+    cards={mock}
+  />).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
