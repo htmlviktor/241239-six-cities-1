f853078e55a9dfe095bd310e5ecc2f761ef6928b
Viktor Kan <viktorkan9@gmail.com>
2019-05-30 00:12:59 +0300

    need create hoc

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import mock from './mocks/offers.js';
 
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const init = () => {
   ReactDOM.render(
